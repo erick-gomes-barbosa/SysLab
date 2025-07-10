f4c0c411986b7f368befa39b75111e5f3d869e8e
erick-gomes-barbosa <erick.barbosa@baymetrics.com.br>
2025-07-10 14:38:04 -0300

    refactor: Adding some DROP commands to eventual maintence

--- Modelagem Física/log/ddl.sql
+++ Modelagem Física/log/ddl.sql
@@ -1,6 +1,7 @@
-DROP SCHEMA IF EXISTS log;
+DROP SCHEMA IF EXISTS log CASCADE;
 CREATE SCHEMA log;
 
+DROP TABLE IF EXISTS log.generic;
 CREATE TABLE log.generic(
     --Chave Primária
     id                  INTEGER                     GENERATED ALWAYS AS IDENTITY,
@@ -25,9 +26,9 @@ CREATE TABLE log.generic(
         REFERENCES system.user(id)
         ON UPDATE RESTRICT
         ON DELETE RESTRICT
-)
+);
 
-DROP TABLE IF EXISTS log.user
+DROP TABLE IF EXISTS log.user;
 CREATE TABLE log.user(
     --Chave Primária
     id          INTEGER GENERATED ALWAYS AS IDENTITY,
@@ -54,9 +55,9 @@ CREATE TABLE log.user(
 
     -- Definição dos Campos Únicos
     CONSTRAINT uq_log_user_generic_id   UNIQUE(generic_id)
-)
+);
 
-DROP TABLE IF EXISTS log.laboratory
+DROP TABLE IF EXISTS log.laboratory;
 CREATE TABLE log.laboratory(
     --Chave Primária
     id              INTEGER GENERATED ALWAYS AS IDENTITY,
@@ -83,8 +84,9 @@ CREATE TABLE log.laboratory(
 
     -- Definição dos Campos Únicos
     CONSTRAINT uq_log_laboratory_generic_id       UNIQUE(generic_id)
-)
+);
 
+DROP TABLE IF EXISTS log.laboratory_per_user;
 CREATE TABLE log.laboratory_per_user(
     --Chave Primária
     id              INTEGER GENERATED ALWAYS AS IDENTITY,
@@ -121,8 +123,9 @@ CREATE TABLE log.laboratory_per_user(
 
     -- Definição dos Campos Únicos
     CONSTRAINT uq_log_laboratory_per_user_generic_id       UNIQUE(generic_id)
-)
+);
 
+DROP TABLE IF EXISTS log.assosiation_user_laboratory;
 CREATE TABLE log.assosiation_user_laboratory(
     --Chave Primária
     id              INTEGER GENERATED ALWAYS AS IDENTITY,
@@ -156,8 +159,9 @@ CREATE TABLE log.assosiation_user_laboratory(
 
     -- Definição dos Campos Únicos
     CONSTRAINT uq_log_assosiation_user_laboratory_generic_id       UNIQUE(generic_id)
-)
+);
 
+DROP TABLE IF EXISTS log.user_login_logout;
 CREATE TABLE log.user_login_logout(
     --Chave Primária
     id              INTEGER GENERATED ALWAYS AS IDENTITY,
@@ -188,4 +192,4 @@ CREATE TABLE log.user_login_logout(
 
     -- Definição dos Campos Únicos
     CONSTRAINT uq_log_user_login_logout_generic_id   UNIQUE(generic_id)
-)
+);
