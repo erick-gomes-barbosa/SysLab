1bbcb228db732bc30cddf3df2f54c0e1f77d9f17
erick-gomes-barbosa <erick.barbosa@baymetrics.com.br>
2025-07-10 14:31:29 -0300

    fix: Adjusting the entry and departure time's domain

--- Modelagem Física/utils/domains.sql
+++ Modelagem Física/utils/domains.sql
@@ -1,8 +1,9 @@
 --Schema SYSTEM
+DROP DOMAIN IF EXISTS utils.domain_time_period;
 CREATE DOMAIN utils.domain_time_period AS TIME(0) WITH TIME ZONE;
 ALTER DOMAIN utils.domain_time_period
 ADD CONSTRAINT check_time_period
 CHECK(
-    (VALUE AT TIME ZONE 'America/Sao_Paulo')::TIME WITHOUT TIME ZONE > '04:00:00' AND
-    (VALUE AT TIME ZONE 'America/Sao_Paulo')::TIME WITHOUT TIME ZONE < '22:00:00'
+    (VALUE AT TIME ZONE 'America/Sao_Paulo')::TIME WITHOUT TIME ZONE >= '04:00:00' AND
+    (VALUE AT TIME ZONE 'America/Sao_Paulo')::TIME WITHOUT TIME ZONE <= '22:00:00'
 );
